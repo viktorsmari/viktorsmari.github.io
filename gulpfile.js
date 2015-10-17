0abb758edfae08c95acc79428d7377a9eea90dad
Viktor Smari <viktorsmari@gmail.com>
2015-10-17 11:26:53 +0200

    Spaces to tabs

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,19 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 
 gulp.task('serve', function() {
-	    browserSync({
-			        server:{
-						            baseDir:"./"
-			        }
-						       })
+	browserSync({
+		server:{
+			baseDir:"./"
+		}
+	})
 });
 
 
 gulp.task('default', function() {
-	    browserSync.init({
-			        server:{
-						            baseDir:'./'
-			        }
-						       });
-		    gulp.watch(['*.html', '*.css', '*.js'], {cwd:'.'}, browserSync.reload);
+	browserSync.init({
+		server:{
+			baseDir:'./'
+			}
+		});
+	gulp.watch(['*.html', '*.css', '*.js'], {cwd:'.'}, browserSync.reload);
 })
